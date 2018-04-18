87f29c70e5c12c0b21578b4a4f023d5dc819800f
Ted Giblette <tedngiblette@gmail.com>
2018-04-18 14:51:19 -0600

    Adds setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""rapidboom: A tool set for rapid boom prediction of parametric geometries."""
+
+from setuptools import setup
+
+setup(name='rapidboom',
+      version='0.0',
+      description='A tool set for rapid boom prediction of parametric geometries.',
+      url='NA',
+      author='usuaero',
+      author_email='doug.hunsaker@usu.edu',
+      license='MIT',
+      packages=['rapidboom'],
+      zip_safe=False)
