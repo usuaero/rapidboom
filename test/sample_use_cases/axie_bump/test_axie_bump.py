e5cd6a07a20a176400d112c93f7890129426b797
Ted Giblette <tedngiblette@gmail.com>
2018-04-27 12:37:35 -0600

    Updates test for Windows

--- test/sample_use_cases/axie_bump/test_axie_bump.py
+++ test/sample_use_cases/axie_bump/test_axie_bump.py
@@ -2,11 +2,23 @@
 
 import pytest
 from rapidboom import AxieBump
+import platform
 
 
 def test_axie_bump_case():
     CASE_DIR = "./test/sample_use_cases/axie_bump/"
-    axiebump = AxieBump(CASE_DIR)
+
+    print(platform.system())
+    if platform.system() == 'Linux':
+        PANAIR_EXE = 'panair'
+        SBOOM_EXE = 'sboom_linux'
+    elif platform.system() == 'Windows':
+        PANAIR_EXE = 'panair.exe'
+        SBOOM_EXE = 'sboom_windows.dat.allow'
+    else:
+        raise RuntimeError("platfrom not recognized")
+
+    axiebump = AxieBump(CASE_DIR, PANAIR_EXE, SBOOM_EXE)
     axiebump.MESH_COARSEN_TOL = 0.00005
     loudness = axiebump.run([0.1, 20., 6.])
 
